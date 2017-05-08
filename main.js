501d79c6854f3ae7a93ac99ee0ae698d8248b369
Kim <kim-bergeron@hotmail.com>
2017-05-08 15:25:41 -0400

    functionality

--- main.js
+++ main.js
@@ -2,8 +2,56 @@ console.log('this is working');
 
 function startGame() {
     document.turn = 'X';
+    setMessage(document.turn + 'gets to start')
 }
 
+function setMessage(msg){
+    document.getElementById('message').innerText = msg;
+}
 function nextMove(square){
+    if(square.innerText === ''){
+    square.innerText = document.turn;
+    switchTurn();    
+    } else {
+        setMessage('Pick another square')
+    }
 
+}
+
+function switchTurn() {
+    if(checkForWinner(document.turn)){
+        setMessage('Congrats ' + document.turn + 'you won')
+    } else if (document.turn === 'X') {
+        document.turn = 'O'
+         setMessage("It's " + document.turn + "'s turn");
+    } else {
+        document.turn = 'X'
+         setMessage("It's " + document.turn + "'s turn");
+    }
+   
+}
+function checkForWinner(move){
+    var result = false;
+    if(checkBox(1,2,3, move) ||
+       checkBox(4,5,6, move) ||
+       checkBox(7,8,9, move) ||
+       checkBox(1,4,6, move) ||
+       checkBox(2,5,8, move) ||
+       checkBox(3,6,9, move) ||
+       checkBox(1,5,9, move) ||
+       checkBox(3,5,7, move) ||){
+           result = true
+       }
+       return result;
+    )
+}
+function checkBox(a, b, c, move){
+    var result = false;
+    if(getBox(a) === move && getBox(b) === move && getBox(c) === move){
+        result = true;
+    }
+    return result;
+}
+function getBox(number){
+    return document.getElementById('s' + number).innerText;
 }
